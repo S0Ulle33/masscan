fa698295e18dfc7d47381c46c86821507907798a
Woolf <29702137+S0Ulle33@users.noreply.github.com>
2020-08-31 17:36:26 +0600

    Update string_s.h

--- src/string_s.h
+++ src/string_s.h
@@ -55,8 +55,8 @@
 
 const char *strerror_x(int x);
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1900)
-/*Visual Studio 2015 and higher*/
+#if defined(_MSC_VER) && (_MSC_VER >= 1600)
+/*Visual Studio 2010 and higher*/
 # include <stdio.h>
 # include <string.h>
 # define strcasecmp     _stricmp
@@ -66,19 +66,6 @@ const char *strerror_x(int x);
 #  define PRId64 "lld"
 #  define PRIx64 "llx"
 # endif
-
-#elif defined(_MSC_VER) && (_MSC_VER == 1600)
-/*Visual Studio 2010*/
-# include <stdio.h>
-# include <string.h>
-# define strcasecmp     _stricmp
-# define memcasecmp     _memicmp
-# ifndef PRIu64
-#  define PRIu64 "llu"
-#  define PRId64 "lld"
-#  define PRIx64 "llx"
-# endif
-
 
 #elif defined(_MSC_VER) && (_MSC_VER == 1200)
 /* Visual Studio 6.0 */
